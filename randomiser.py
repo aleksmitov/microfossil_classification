041d509c37a083071ac989710a482eb54645366b
Aleksandar <alex.mitov96@gmail.com>
2018-04-08 16:08:07 +0100

    completed randomiser.py

--- randomiser.py
+++ randomiser.py
@@ -34,3 +34,47 @@ def split_files_into_groups(dir_path, N):
             shutil.copy2(os.path.join(dir_path, selected_file), os.path.join(target_dir, selected_file))
             # print("Copying: {} into {}".format(os.path.join(dir_path, selected_file),
             #                                   os.path.join(target_dir, selected_file)))
+
+
+def random_files_subset(dir_path, select_percent):
+    """
+    Randomly selects files from the dir_path
+    :param dir_path: string with the dir path holding the files
+    :param select_percent: a number in the range [0, 100]
+    :return: None
+    """
+    files = [file for file in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, file))]
+    available_files = len(files)
+    to_select = int(round(len(files) * select_percent * 1.0 / 100))
+    target_dir = os.path.join(dir_path, "subset_{}_percent".format(select_percent))
+    if os.path.isdir(target_dir) is False:
+        os.makedirs(target_dir)
+
+    for i in range(0, to_select):
+        random_file_index = random.randint(0, available_files - 1)  # Select a random available file
+        selected_file = files[random_file_index]
+        # Now swap selected file to put it at the end
+        files[random_file_index], files[available_files - 1] = files[available_files - 1], files[random_file_index]
+        # print("Selected index {}, now swapping indexes {} and {}".format(random_file_index,
+        #                                                                 random_file_index, available_files-1))
+        available_files -= 1
+
+        # Now copy the file into the target dir
+        shutil.copy2(os.path.join(dir_path, selected_file), os.path.join(target_dir, selected_file))
+        # print("Copying: {} into {}".format(os.path.join(dir_path, selected_file),
+        #                                   os.path.join(target_dir, selected_file)))
+
+
+def main():
+    target_dir = "./exp_split"
+
+    n_groups = 7
+    print("Splitting files into {} groups:".format(n_groups))
+    split_files_into_groups(target_dir, n_groups)
+
+    #to_select_percent = 91
+    #print("Random subset:")
+    #random_files_subset(target_dir, to_select_percent)
+
+
+main()
