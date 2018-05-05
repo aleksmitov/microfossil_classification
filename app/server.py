be7ea43ab2d5eff09fcefa8c783294811338a294
Aleksandar <alex.mitov96@gmail.com>
2018-05-05 12:14:17 +0100

    saving progress

--- app/server.py
+++ app/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, flash
 from werkzeug.utils import secure_filename
 import os
 import cv2
@@ -9,6 +9,7 @@ import tensorflow as tf
 import functools
 import tempfile
 import uuid
+from PIL import Image
 
 os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import extract_microfossils
@@ -25,42 +26,73 @@ CROP_DIMS = (200, 200)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "./static/uploads"
+app.secret_key = "secret_key"
 
 @app.route("/")
 def index():
-    start_time = time.time()
+    return "Index!"
 
-    metadata = Metadata(METADATA_FILE)
-    model_path = os.path.join("..", (os.path.join(metadata.models_dir, MODEL_TO_LOAD)))
-    image_file = "./test.png"
-    unfiltered_predictions, filtered_predictions = predict.extract_and_classify_image(image_file, metadata,
-                                                                    model_path, MIN_MICROFOSSIL_SIZE, CROP_DIMS, True)
+
+@app.route("/extract_and_classify_single_image", methods=["GET", "POST"])
+def extract_and_classify():
+    start_time = time.time()
     unfiltered_images_paths_predictions = []
     filtered_images_paths_predictions = []
-    for crop, crop_predictions in unfiltered_predictions:
-        unique_filename = str(uuid.uuid4()) + ".png"
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
-        cv2.imwrite(file_path, crop)
-        link_to_file = url_for("static", filename="uploads/{}".format(unique_filename), _external=True)
-        unfiltered_images_paths_predictions.append((link_to_file, crop_predictions.tolist()))
+    uploaded_image_link = None
 
-    for crop, crop_predictions in filtered_predictions:
-        unique_filename = str(uuid.uuid4()) + ".png"
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
-        cv2.imwrite(file_path, crop)
-        link_to_file = url_for("static", filename="uploads/{}".format(unique_filename), _external=True)
-        filtered_images_paths_predictions.append((link_to_file, crop_predictions.tolist()))
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "image" not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files["image"]
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and is_file_allowed(file.filename, ALLOWED_EXTENSIONS):
+            extension = file_extension(file.filename)
+            unique_filename = str(uuid.uuid4()) + ".png"
+            image_object = Image.open(file.stream)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
+            # Also converts to desired image format
+            image_object.save(file_path)
+            #file.save(file_path)
+        metadata = Metadata(METADATA_FILE)
+        model_path = os.path.join("..", (os.path.join(metadata.models_dir, MODEL_TO_LOAD)))
+        image_file = "./test.png"
+        unfiltered_predictions, filtered_predictions = predict.extract_and_classify_image(image_file, metadata,
+                                                                        model_path, MIN_MICROFOSSIL_SIZE, CROP_DIMS, True)
+
+        for crop, crop_predictions in unfiltered_predictions:
+            unique_filename = str(uuid.uuid4()) + ".png"
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
+            cv2.imwrite(file_path, crop)
+            link_to_file = url_for("static", filename="uploads/{}".format(unique_filename), _external=True)
+            unfiltered_images_paths_predictions.append((link_to_file, crop_predictions.tolist()))
+
+        for crop, crop_predictions in filtered_predictions:
+            unique_filename = str(uuid.uuid4()) + ".png"
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
+            cv2.imwrite(file_path, crop)
+            link_to_file = url_for("static", filename="uploads/{}".format(unique_filename), _external=True)
+            filtered_images_paths_predictions.append((link_to_file, crop_predictions.tolist()))
 
     elapsed_time = time.time() - start_time
     return render_template("image_extraction_and_classification.html",
                                     unfiltered_predictions=unfiltered_images_paths_predictions,
-                                    filtered_predictions=filtered_images_paths_predictions)
-    #return "Tempdir: {}, Elapsed time: {}\n{} ||| {}".format(tempfile.gettempdir(), elapsed_time,
-    #                                                  unfiltered_images_paths_predictions, filtered_images_paths_predictions)
+                                    filtered_predictions=filtered_images_paths_predictions,
+                                    uploaded_image_link=uploaded_image_link,
+                                    elapsed_time=elapsed_time)
 
 
 def is_file_allowed(filename, allowed_extensions):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+
+def file_extension(filename):
+    return filename.rsplit('.', 1)[1].lower()
 
 
 
